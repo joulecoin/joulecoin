f296d59d21caf26883b1ce6911f5d4b8882517df
joulecoin <joulecoin@noreply.users.github.com>
2016-05-20 07:21:15 +0200

    version 0.9.2.5

--- src/clientversion.h
+++ src/clientversion.h
@@ -12,14 +12,14 @@
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       9
 #define CLIENT_VERSION_REVISION    2
-#define CLIENT_VERSION_BUILD       4
+#define CLIENT_VERSION_BUILD       5
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2015
+#define COPYRIGHT_YEAR 2016
 
 #endif //HAVE_CONFIG_H
 
